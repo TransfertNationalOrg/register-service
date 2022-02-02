f0914d37d37f3c4abbc337237098f1b9effb0c27
ambou-97 <ambroise.tine@gmail.com>
2022-02-02 02:29:46 +0100

    exceptions

---
+++ src/main/java/ma/ensa/exception/DuplicatedException.java
@@ -0,0 +1,14 @@
+package ma.ensa.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+public class DuplicatedException extends Exception{
+
+    private Long id;
+
+}
