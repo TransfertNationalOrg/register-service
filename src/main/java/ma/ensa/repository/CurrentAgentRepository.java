7918ca1bfc455673cfd3d856db07d30ee6cc6594
ambou-97 <ambroise.tine@gmail.com>
2022-02-04 14:44:04 +0100

    register

--- src/main/java/ma/ensa/repository/CurrentAgentRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.ensa.repository;
-
-import ma.ensa.model.CurrentAgent;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface CurrentAgentRepository extends JpaRepository<CurrentAgent, Long> {
-}
